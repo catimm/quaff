0fea0c4588ed738a6159145a31348c1a8f1029ad
Carl Timm <ctiv@hotmail.com>
2015-04-12 22:56:29 -0700

    Playing with checkbox filters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def ratings_sort(title, rating_type)
     link_to title, :ratings_sort => rating_type
   end
+  
+  def checkbox_sort(type, name)
+    check_box_tag "type[#{name}]", name, false
+  end
 end
