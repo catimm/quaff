8d6dfcc3fd95fcfbaaaa53ab3958d1744cb6607c
catimm <ctiv@hotmail.com>
2015-05-10 21:50:05 -0700

    Added code to deal with nil beer ratings

--- app/controllers/concerns/best_guess.rb
+++ app/controllers/concerns/best_guess.rb
@@ -20,16 +20,31 @@ module BestGuess
           this_beer_style_id = BeerType.where(id: this_beer.beer_type_id).pluck(:beer_style_id)[0]
           # Rails.logger.debug("This beer's style id: #{this_beer_style_id.inspect}")
           if @user_style_likes.include? this_beer_style_id
-            # this "formula" is used if the user generally likes this beer style--multiply default rating by 1.05
-            this_beer.best_guess = ((((((this_beer.beer_rating * 0.9)*1.05)*2)*2).round)/2.0)
+            if !this_beer.beer_rating.nil?
+              # this "formula" is used if the user generally likes this beer style--multiply default rating by 1.05
+              this_beer.best_guess = ((((((this_beer.beer_rating * 0.9)*1.05)*2)*2).round)/2.0)
+            else
+              this_beer.beer_rating = 3.5
+              this_beer.best_guess = ((((((this_beer.beer_rating * 0.9)*1.05)*2)*2).round)/2.0)
+            end
             # Rails.logger.debug("User likes style--rating: #{this_beer.best_guess.inspect}")
           elsif @user_style_dislikes.include? this_beer_style_id
-            # this "formula" is used if the user generally dislikes this beer style--multiply default rating by 0.8
-            this_beer.best_guess = ((((((this_beer.beer_rating * 0.9)*0.8)*2)*2).round)/2.0)
+            if !this_beer.beer_rating.nil?
+              # this "formula" is used if the user generally dislikes this beer style--multiply default rating by 0.8
+              this_beer.best_guess = ((((((this_beer.beer_rating * 0.9)*0.8)*2)*2).round)/2.0)
+            else
+              this_beer.beer_rating = 3.5
+              this_beer.best_guess = ((((((this_beer.beer_rating * 0.9)*0.8)*2)*2).round)/2.0)
+            end
             # Rails.logger.debug("User dislikes style--rating: #{this_beer.best_guess.inspect}")
           else 
-            # this "formula" is the default if we know nothing about the user--mulitply public rating by 0.9
-            this_beer.best_guess = (((((this_beer.beer_rating * 0.9)*2)*2).round)/2.0)
+            if !this_beer.beer_rating.nil?
+              # this "formula" is the default if we know nothing about the user--mulitply public rating by 0.9
+              this_beer.best_guess = (((((this_beer.beer_rating * 0.9)*2)*2).round)/2.0)
+            else
+              this_beer.beer_rating = 3.5
+              this_beer.best_guess = (((((this_beer.beer_rating * 0.9)*2)*2).round)/2.0)
+            end
             # Rails.logger.debug("default formula--in if/else: #{this_beer.best_guess.inspect}")
           end
           # if this beer hasn't yet been associated with a beer type/style, use the default formula
