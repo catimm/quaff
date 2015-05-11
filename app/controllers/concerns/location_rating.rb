10848e1c9fc4f58484980bda7a8a4cf54fdcf305
catimm <ctiv@hotmail.com>
2015-05-10 23:08:39 -0700

    Code test

--- app/controllers/concerns/location_rating.rb
+++ app/controllers/concerns/location_rating.rb
@@ -9,7 +9,7 @@ module LocationRating
       Rails.logger.debug("Beer ids: #{@beer_ids.inspect}")
       @beer_ranking = best_guess(@beer_ids).sort_by(&:best_guess).reverse
       Rails.logger.debug("New Beer info: #{@beer_ranking.inspect}")
-      @rank_input = @beer_ranking.first(5)
+      @rank_input = @beer_ranking
       Rails.logger.debug("Beer ranks: #{@rank_input.inspect}")
       # create list of top 5 beers per location
       if @rank_input[0].beer_type.nil?
