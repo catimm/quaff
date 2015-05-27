17278c7ba6e19335b3aea277865be1b85b014d2b
catimm <ctiv@hotmail.com>
2015-05-26 23:37:57 -0700

    Added devise sanitizer for username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :role_id
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
   
   private
