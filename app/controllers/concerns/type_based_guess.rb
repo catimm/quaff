0c2ef892fa7d13bf215ee230fc32915878ec2b3f
catimm <ctiv@hotmail.com>
2015-06-17 14:02:21 -0700

    Debugging

--- app/controllers/concerns/type_based_guess.rb
+++ app/controllers/concerns/type_based_guess.rb
@@ -2,6 +2,6 @@ module TypeBasedGuess
   extend ActiveSupport::Concern
   
   def type_based_guess(this_beer)
-    this_beer.best_guess = 10
+    this_beer.best_guess = 8
   end # end of method
 end # end of module
