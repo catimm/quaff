aca0b64f0ec915725908bb75393c188eaf79f3cc
catimm <ctiv@hotmail.com>
2015-06-21 23:22:22 -0700

    Added user notification preference on sign-up

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -19,6 +19,31 @@ class InvitationsController < Devise::InvitationsController
   end
   
   def update
-    super
+    raw_invitation_token = update_resource_params[:invitation_token]
+    self.resource = accept_resource
+    invitation_accepted = resource.errors.empty?
+
+    yield resource if block_given?
+
+    if invitation_accepted
+      # add default notification preferences for new user
+      @default_notification_preference = UserNotificationPreference.new(user_id: resource.id, notification_one: "user high rating availability notice",
+                                          preference_one: true, threshold_one: 9.5, notification_two: "highly recommended availability notice",
+                                          preference_two: true, threshold_two: 9.5)
+      @default_notification_preference.save!
+      Rails.logger.debug("Resource info: #{resource.inspect}")
+      if Devise.allow_insecure_sign_in_after_accept
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message :notice, flash_message if is_flashing_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_accept_path_for(resource)
+      else
+        set_flash_message :notice, :updated_not_active if is_flashing_format?
+        respond_with resource, :location => new_session_path(resource_name)
+      end
+    else
+      resource.invitation_token = raw_invitation_token
+      respond_with_navigational(resource){ render :edit }
+    end
   end
 end
