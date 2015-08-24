9f2912889c6a64e531976237f4b60737b7156f73
catimm <ctiv@hotmail.com>
2015-08-24 12:35:05 -0700

    Adding config files for Heroku Searchbox

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Config Elasticsearch for Searchkick
+  Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
 end
