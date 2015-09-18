970c6b4c04e17c9df17ca255703ea635f1188bb9
catimm <ctiv@hotmail.com>
2015-09-18 11:39:31 -0700

    Debugging

--- app/controllers/concerns/location_rating.rb
+++ app/controllers/concerns/location_rating.rb
@@ -8,12 +8,12 @@ module LocationRating
       @beer_ids = BeerLocation.where(location_id: this_location.id, beer_is_current: "yes").pluck(:beer_id)
       # Rails.logger.debug("Beer ids: #{@beer_ids.inspect}")
       @beer_ranking = best_guess(@beer_ids).sort_by(&:ultimate_rating).reverse
-      # Rails.logger.debug("New Beer info: #{@beer_ranking.inspect}")
+      Rails.logger.debug("New Beer info: #{@beer_ranking.inspect}")
       @rank_input = @beer_ranking.first(5)
-      # Rails.logger.debug("Beer ranks: #{@rank_input.inspect}")
+      Rails.logger.debug("Beer ranks: #{@rank_input.inspect}")
       # create list of top 5 beers per location
       @rank_input.each_with_index do |i, index|
-        # Rails.logger.debug("This beer info: #{i.best_guess.inspect}")
+        Rails.logger.debug("This beer info: #{i.best_guess.inspect}")
         if i.beer_type_id.nil?
           @beer_type_name = ""
         else 
