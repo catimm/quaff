614b89f0d0e7d222fd0a88cad0a2af6e910e1881
catimm <ctiv@hotmail.com>
2015-09-21 22:03:31 -0700

    More iframe work

--- public/draft.js
+++ public/draft.js
@@ -6,8 +6,18 @@ window.onload = function() {
 
    //iFrame
    var iframe = document.createElement('iframe');
-   iframe.style.display = "none";
+   iframe.name = "draft-frame";
+   iframe.style.border = "0";
+   iframe.style.frameborder = "0";
+   iframe.style.cellspacing = "0";
    iframe.style.width = "100%";
    iframe.src = "https://quaff-stage.herokuapp.com/draft/" + id;
    document.body.appendChild(iframe);
+   
+   //css
+   var cssLink = document.createElement("link");
+   cssLink.href = "/assets/application-15d9bb5a37036843a6caa5de5da2b0e6.css";
+   cssLink .rel = "stylesheet";
+   cssLink .type = "text/css";
+   frames['draft-frame'].document.head.appendChild(cssLink);
 };
