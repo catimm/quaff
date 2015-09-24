893ce5fa89b6dafc57fac9278c49b831ccb43088
catimm <ctiv@hotmail.com>
2015-09-24 11:26:32 -0700

    iframe

--- public/draft.js
+++ public/draft.js
@@ -8,7 +8,6 @@ window.onload = function() {
    var iframe = document.createElement('iframe');
    iframe.name = "draft-frame";
    iframe.id = "draft-frame";
-   iframe.setAttribute('onload', 'testing()'); // assign an id
    iframe.style.border = "0";
    iframe.style.frameborder = "0";
    iframe.style.cellspacing = "0";
@@ -24,7 +23,7 @@ window.onload = function() {
    document.body.appendChild(iframe_script_one);
    var iframe_script_two = document.createElement('script');
    iframe_script_two.type = "text/javascript";
-   iframe_script_two.innerHTML = "iFrameResize({log:true})";
+   iframe_script_two.innerHTML = "iFrameResize({log:true});";
    document.body.appendChild(iframe_script_two);
    
    //css
