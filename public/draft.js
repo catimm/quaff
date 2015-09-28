d9aea4233a1a212c1706eca47e5f775882cf69d5
catimm <ctiv@hotmail.com>
2015-09-28 11:00:28 -0700

    iframe

--- public/draft.js
+++ public/draft.js
@@ -3,20 +3,18 @@ window.onload = function() {
    //Params
    var scriptPram = document.getElementById('load_widget');
    var id = scriptPram.getAttribute('data-page');
-	
-	
-   
+ 
    //iFrame
-   //var iframe = document.createElement('iframe');
-   //iframe.name = "draft-frame";
-   //iframe.id = "draft-frame";
-   //iframe.style.border = "0";
-   //iframe.style.frameborder = "0";
-   //iframe.style.cellspacing = "0";
-   //iframe.style.width = "100%";
-   //iframe.scrolling = "no";
-   //iframe.src = "https://quaff-stage.herokuapp.com/draft/" + id;
-   //document.body.appendChild(iframe);
+   var iframe = document.createElement('iframe');
+   iframe.name = "draft-frame";
+   iframe.id = "draft-frame";
+   iframe.style.border = "0";
+   iframe.style.frameborder = "0";
+   iframe.style.cellspacing = "0";
+   iframe.style.width = "100%";
+   iframe.scrolling = "no";
+   iframe.src = "https://quaff-stage.herokuapp.com/draft/" + id;
+   document.body.appendChild(iframe);
 
 	//jquery
 	var jquery = document.createElement('script');
@@ -31,7 +29,7 @@ window.onload = function() {
    document.body.appendChild(iframe_script_one);
    var iframe_script_two = document.createElement('script');
    iframe_script_two.type = "text/javascript";
-   iframe_script_two.innerHTML = "var transport = new easyXDM.Socket({ remote: 'https://quaff-stage.herokuapp.com/draft/" + id +"', remoteHelper: 'https://quaff-stage.herokuapp.com/name.html', swf: 'https://quaff-stage.herokuapp.com/easyxdm.swf', container: 'container', onMessage: function (message, origin) { this.container.getElementsByTagName('iframe')[0].style.height = message + 'px';} });";
+   iframe_script_two.innerHTML = "var transport = new easyXDM.Socket({ remote: 'https://quaff-stage.herokuapp.com/draft/" + id +"', remoteHelper: 'https://quaff-stage.herokuapp.com/name.html', swf: 'https://quaff-stage.herokuapp.com/easyxdm.swf', container: 'container', onMessage: function (message, origin) { this.container.getElementsByTagName('draft-frame')[0].style.height = message + 'px';} });";
    document.body.appendChild(iframe_script_two);
    
    //css
