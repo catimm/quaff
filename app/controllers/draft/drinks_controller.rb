c057008253769c6802f44919eaf3156ba985bfd8
catimm <ctiv@hotmail.com>
2015-11-02 14:28:14 -0800

    More updates to Retailer draft board UI/UX

--- app/controllers/draft/drinks_controller.rb
+++ app/controllers/draft/drinks_controller.rb
@@ -17,30 +17,49 @@ class Draft::DrinksController < ApplicationController
     @last_draft_board_update = BeerLocation.where(draft_board_id: @draft_board.id, beer_is_current: "yes").order(:updated_at).reverse_order.first 
     
     # determine whether a drink size column shows in row view
+    @total_number_of_sizes = 0
     @taster_size = 0
     @tulip_size = 0
     @pint_size = 0
     @half_growler_size = 0
     @growler_size = 0
     @beer_location_ids = @current_draft_board.pluck(:id)
-    @drink_details = DraftDetail.where(beer_location_id: @beer_location_ids)
-    @drink_details.each do |details|
-      if details.drink_size > 0 && details.drink_size <= 5
-        @taster_size += 1
+    @current_draft_board.each do |draft_drink|
+      @drink_details = DraftDetail.where(beer_location_id: draft_drink.id)
+      @this_number_of_sizes = 0
+      @drink_details.each do |details|
+        if details.drink_size > 0 && details.drink_size <= 5
+          @taster_size += 1
+          @this_number_of_sizes += 1
+        end
+        if details.drink_size > 5 && details.drink_size <= 12
+          @tulip_size += 1
+          @this_number_of_sizes += 1
+        end
+        if details.drink_size > 12 && details.drink_size <= 22
+          @pint_size += 1
+          @this_number_of_sizes += 1
+        end
+        if details.drink_size == 32
+          @half_growler_size += 1
+          @this_number_of_sizes += 1
+        end
+        if details.drink_size == 64
+          @growler_size += 1
+          @this_number_of_sizes += 1
+        end
+        if @this_number_of_sizes > @total_number_of_sizes
+          @total_number_of_sizes = @this_number_of_sizes
+        end
       end
-      if details.drink_size > 5 && details.drink_size <= 12
-        @tulip_size += 1
+      # set width of columns that hold drink graphics and info
+      if @total_number_of_sizes <= 4
+        @column_class = "col-sm-3"
+        @column_class_xs = "col-xs-3"
+      else
+        @column_class = "col-sm-4"
+        @column_class_xs = "col-xs-4"
       end
-      if details.drink_size > 12 && details.drink_size <= 22
-        @pint_size += 1
-      end
-      if details.drink_size == 32
-        @half_growler_size += 1
-      end
-      if details.drink_size == 64
-        @growler_size += 1
-      end
-    end
   end
   
   def allow_iframe_requests
