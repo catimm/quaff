7fed1d284f5061a0ce4b92232afe387ff5647c90
catimm <ctiv@hotmail.com>
2016-08-22 13:31:47 -0700

    Change log_in_path in applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       super
     else
-      redirect_to login_path, :notice => 'Please log in first!'
+      redirect_to new_user_session_path, :notice => 'Please log in first!'
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
