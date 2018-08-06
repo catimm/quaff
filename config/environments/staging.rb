40d51290cd39780c9a8d5ac9c0ff5e0172c427fa
Carl Timm <ctiv@hotmail.com>
2018-08-05 22:43:33 -0700

    More debugging for staging server

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -48,6 +48,7 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger.level = Logger::DEBUG
   end
   
   # Prepend all log lines with the following tags.
