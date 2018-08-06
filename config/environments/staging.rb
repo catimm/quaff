3d4421200f47d695bfde2564bfa6fb3ea8ca013c
Carl Timm <ctiv@hotmail.com>
2018-08-05 22:07:43 -0700

    Edits to logging setup in staging environment file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -43,7 +43,13 @@ Rails.application.configure do
 
   # Set to :debug to see everything in the log; :info to see high-level.
   config.log_level = :debug
-
+  
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+  
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
